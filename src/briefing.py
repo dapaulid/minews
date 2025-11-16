f22e964f237dbeb7f146cc45c12c637b82cd4343
Daniel Pauli <dapaulid@gmail.com>
2025-11-16 12:57:17 +0100

    Fix timezone.

--- src/briefing.py
+++ src/briefing.py
@@ -2,22 +2,24 @@ from article import Article
 from datetime import datetime
 import rate
 
+from babel.dates import format_datetime as babel_format_datetime
+
 def format_md(articles: list[Article]) -> str:
     
     # split articles into important and unimportant
     important = [a for a in articles if rate.is_important(a)]
     unimportant = [a for a in articles if not rate.is_important(a)]
 
-    md = f"_Updated: {datetime.now().strftime("%c")}_\n\n"
+    md = f"_Aktualisiert: {format_datetime(datetime.now())}_\n\n"
 
     # output important articles first
     if important:
         md += format_articles(important)
     else:
-        md += "Nothing important happened in the last 12 hours.\n"
+        md += "Keine wichtigen Ereignisse in den letzten 12 Stunden.\n"
     # output unimportant articles in a collapsible section
     if unimportant:
-        md += "\n<details>\n<summary>Show unimportant news</summary>\n\n"
+        md += "\n<details>\n<summary>Unwichtige Ereignisse anzeigen</summary>\n\n"
         md += format_articles(unimportant)
         md += "\n</details>\n"
 
@@ -27,8 +29,12 @@ def format_articles(articles: list[Article]) -> str:
     md = ""
     for article in articles:
         md += f"## {article.title}\n\n"
-        md += f"[{article.source}]({article.url}), {article.publishedAt.strftime('%c')}\n\n"
+        md += f"[{article.source}]({article.url}) • _{format_datetime(article.publishedAt)}_\n\n"
         md += f"{article.description}\n\n"
         md += f"{article.content}\n"
         md += f"\n_Relevance score: [{article.score}] - {article.reasoning}_\n"
     return md
+
+def format_datetime(dt: datetime) -> str:
+    # TODO localization
+    return babel_format_datetime(dt, locale="de_CH", tzinfo="Europe/Zurich", format="EEEE, d. MMMM YYYY, hh:mm") + " Uhr"
